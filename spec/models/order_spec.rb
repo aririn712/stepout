f392693bf3946f4fdb8c129f5b2be5ff9b04c275
aririn712 <atnil012@icloud.com>
2021-09-15 17:43:39 +0900

    モデルのテストコード

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  before do
+    user = FactoryBot.create(:user)
+    course = FactoryBot.build(:course)
+    course.save
+    @order = FactoryBot.build(:order, user_id: user.id, course_id: course.id)
+  end
+
+  describe '受講申請' do
+    context '申請できる時' do
+      it '紐付けできてる時' do
+        expect(@order).to be_valid
+      end
+    end
+
+    context '申請できない時' do
+      it 'user_idが空だと申請できないこと' do
+        @order.user_id = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("User must exist")
+      end
+      it 'course_idが空だと申請できないこと' do
+        @order.course_id = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Course must exist")
+      end
+    end
+  end
+end
