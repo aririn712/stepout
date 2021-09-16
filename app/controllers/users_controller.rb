4b043ac9b359a0ee3dcbc5218924708115afd688
aririn712 <atnil012@icloud.com>
2021-09-16 10:01:59 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @courses = @user.courses
-
-    orders = Order.where(user_id: current_user.id).pluck(:course_id)
-    @order_courses = Course.find(orders)
+    if user_signed_in?
+      orders = Order.where(user_id: current_user.id).pluck(:course_id)
+      @order_courses = Course.find(orders)
+    end
   end
 
   def edit
